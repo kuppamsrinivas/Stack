ea854b67748f81cd93df0e200732a6c0b277dab2
surendraguna <surendraguna2812@gmail.com>
2023-10-26 20:07:45 +0530

    Implementation of stack is done

--- Stack.java
+++ Stack.java
@@ -14,27 +14,62 @@ class Stack{
     static Node top = null;
     
     void push(int data){
-
+        Node n = new Node(data); // Create a Node
+        if(top == null) // top == null -> top == n.
+        {
+            top = n;
+            return;
+        }
+        n.next = top; // New Node next is top
+        top = n; // top = new Node
     }
 
     void pop(){
-
+        if(top == null)
+        {
+            System.out.println("UnderFlow");
+            return;
+        }
+        top = top.next; // Change the top
     }
 
     void display(){
-
+        if(top == null)
+        {
+            System.out.println("UnderFlow");
+            return;
+        }
+        Node temp = top;
+        while (temp != null)
+        {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 
     int search(int i){
-
+        Node temp = top;
+       int c = 1;
+       while (temp != null)
+       {
+           if (temp.data == i)
+               return c;
+           c++;
+           temp = temp.next;
+       }
+       return -1;      
     }
 
     int peek(){
-
+        if(top == null)
+            return -1;
+        return top.data;
     }
 
     boolean empty(){
-
+        if(top == null)
+            return true;
+        return false;
     }
     public static void main(String[] args)
     {
